b4c4b6d4ff484eec1789737cca558116fd4c7d81
oreo12411 <110176625+yuusyaisami@users.noreply.github.com>
2023-05-27 16:42:19 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace download
+{
+    partial class installer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MainF = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ダウンロードウィザード";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Atomo v1.4.5 (64bit)をダウンロードする";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(471, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "download";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(597, 417);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Close";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::download.Properties.Resources.ATOMo;
+            this.pictureBox1.Location = new System.Drawing.Point(536, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(243, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainF
+            // 
+            this.MainF.Enabled = true;
+            this.MainF.Interval = 20;
+            this.MainF.Tick += new System.EventHandler(this.MainF_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(34, 359);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(728, 23);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Visible = false;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(64, 340);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(186, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "アプリケーションをダウンロード中";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(533, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "0";
+            this.label6.Visible = false;
+            // 
+            // installer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "installer";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer MainF;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace download
+{
+    public partial class installer : Form
+    {
+        public installer()
+        {
+            InitializeComponent();
+        }
+        int ml3e = 0;
+        Random r = new Random();
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.LightGray;
+            if(ml3e == 0 && r.Next() % 10 != 0)
+            {
+                ml3e = 1;
+                label3.Location = new Point(label3.Location.X - 200, label3.Location.Y);
+            }
+            else if(ml3e == 1)
+            {
+                ml3e = 0;
+                label3.Location = new Point(471, 417);
+            }
+
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.BackColor = SystemColors.Menu;
+        }
+        int count = 1;
+        private void MainF_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if (progressBar1.Visible == true)
+            {
+                if (count % 600 == 0)
+                {
+                    progressBar1.Value = 85;
+                }
+                if (count % 700 == 0)
+                {
+                    progressBar1.Value = 0;
+                }
+                if (count % 2 == 0)
+                {
+                    progressBar1.Value++;
+                }
+                if (count % 100 == 0)
+                {
+                    progressBar1.Value = progressBar1.Value + 10;
+                }
+                if (progressBar1.Value >= 10)
+                {
+                    if (count % 20  == 0)
+                    {
+                        progressBar1.Value -= 5;
+                    }
+                    if (count % 10 == 0)
+                    {
+                        progressBar1.Value -= 1;
+                    }
+                    if (count % 1 == 0)
+                    {
+                        progressBar1.Value += 1;
+                    }
+                    if (count % 1000 == 0)
+                    {
+                        progressBar1.Value = 1;
+                    }
+                }
+                if (progressBar1.Value > 50 && progressBar1.Value < 79)
+                {
+                    progressBar1.Value++;
+                    if (count % 10 == 0)
+                    {
+                        progressBar1.Value -= 10;
+                    }
+                    else if (count % 15 == 0)
+                    {
+                        progressBar1.Value += 20;
+                    }
+                }
+                if (progressBar1.Value > 90)
+                {
+                        progressBar1.Value -= 50;
+                }
+                label6.Text = count / 3600 + "時" + count / 60 + "分" + count % 60 + "秒後にダウンロードは終了します";
+            }
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = true;
+            label6.Visible = true;
+
+
+        }
+    }
+}
